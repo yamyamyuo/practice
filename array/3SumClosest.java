e639d2bbe12299419c2aa92b335e2e74405a812a
bjlinmanna <bjlinmanna@corp.netease.com>
2017-02-14 19:43:37 +0800

    3sumclosest

--- array/3SumClosest.java
+++ array/3SumClosest.java
@@ -1,5 +1,29 @@
-c class Solution {
+public class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        
+        Arrays.sort(nums);
+	int res = Integer.MAX_VALUE;
+	if (nums.length <= 3) {
+		return nums[0] + nums[1] + nums[2]; 
+	}	
+	for (int i = 0; i < nums.length; i++) {
+		int p = i+1;
+		int q = nums.length - 1;
+		int sum = nums[i] + nums[p] + nums[q];
+	        while(p<q) {	
+		if (sum == target) {
+			return sum;
+		}
+		if (Math.abs(target - res) > Math.abs(target - sum)) {
+			res = sum;
+		}
+		
+		if (sum > target) {
+			q--;
+		} else if (sum < target) {
+			p++;
+		}
+		
+		}
+	}
     }
 }
